3f95cddfb1d3ab3270fcf9bbdc7cd97f5bedcf15
a18antsv <gotneifhd2@gmail.com>
2019-05-02 10:50:16 +0200

    Initial commit

---
+++ app/src/main/java/com/github/a18antsv/medialibraryapp/DataContract.java
@@ -0,0 +1,12 @@
+package com.github.a18antsv.medialibraryapp;
+
+import android.provider.BaseColumns;
+
+public class DataContract {
+
+    private DataContract() {}
+
+    public static class MountainEntry implements BaseColumns {
+
+    }
+}
---
+++ app/src/main/java/com/github/a18antsv/medialibraryapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.github.a18antsv.medialibraryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.test:
+                Toast.makeText(getApplicationContext(), "TEST", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
